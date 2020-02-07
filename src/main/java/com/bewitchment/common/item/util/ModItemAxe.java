95eff3859224cabb2752c20b6b16e0e880370134
Sunconure11 <eyespot7b@aol.com>
2020-02-07 18:09:57 -0500

    Commit changes for 1.15.2 branch

--- src/main/java/com/bewitchment/common/item/util/ModItemAxe.java
+++
@@ -1,9 +0,0 @@
-package com.bewitchment.common.item.util;
-
-import net.minecraft.item.ItemAxe;
-
-public class ModItemAxe extends ItemAxe {
-	public ModItemAxe(ToolMaterial mat) {
-		super(mat, 5 + mat.getAttackDamage(), -3.1f);
-	}
-}
